694d5c0d12352a2db6869f35db494c78e55d3466
Jose Calvillo <josecalvilloolmedo@gmail.com>
2019-05-26 18:12:21 +0200

    Página de team scrims

--- cod/teamscrim.php
+++ cod/teamscrim.php
@@ -1,24 +1,30 @@
 <?php include 'includes/head.php';?>
 <?php
 $correo = $_SESSION['correo'];
+
+// Listamos las team scrims disponibles
+try {
+    $opc = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+    $dsn = "mysql:host=localhost;dbname=esports_finder";
+    $conex = new PDO($dsn, "root", "", $opc);
+    } catch (Exception $ex) {
+      die("Error: ".$e->getMessage());
+    }
+
+    $listar = $conex->prepare("SELECT ts.fecha, e.nombre FROM team_scrim_cod As ts INNER JOIN equipo_cod AS e WHERE ts.id_equipo = e.id_equipo ORDER BY ts.id_teamscrim DESC");
+    $listar->execute();
+
+
 // Creamos una consulta para crear equipo
 if (isset($_POST['proponerts'])) {
     // Recogemos los datos necesarios
     $fecha = $_POST['date'];
-    $hora = $_POST['time'];
  
 
     // Hacemos la consulta
-    if (empty($fecha) || empty($hora)) {
+    if (empty($fecha)) {
         echo "<script type=\"text/javascript\">alert(\"Debes introducir los datos correctamente\");</script>";
     } else {
-        try {
-            $opc = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
-            $dsn = "mysql:host=localhost;dbname=esports_finder";
-            $conex = new PDO($dsn, "root", "", $opc);
-        } catch (Exception $ex) {
-          die("Error: ".$e->getMessage());
-        }
 
         // Obtenemos los datos del equipo según el id
         $sql = $conex->prepare("SELECT * FROM equipo_cod WHERE id_equipo=(SELECT id_equipo FROM cod WHERE created_by='$correo')");
@@ -27,7 +33,7 @@ if (isset($_POST['proponerts'])) {
         $id_equipo = $resultado['id_equipo'];
 
         // Insertamos la ts en la base de datos
-        $sql = $conex->prepare("INSERT INTO `team_scrim_cod`(`fecha`, `hora`, `id_equipo`, `created_by`, `modified_by`) VALUES ('".$fecha."','".$hora."','".$id_equipo."','".$correo."','".$correo."')");
+        $sql = $conex->prepare("INSERT INTO `team_scrim_cod`(`fecha`, `id_equipo`, `created_by`, `modified_by`) VALUES ('".$fecha."','".$id_equipo."','".$correo."','".$correo."')");
         $sql->execute();
         unset($conex);
         header("Location: teamscrim.php");
@@ -41,8 +47,21 @@ if (isset($_POST['proponerts'])) {
 <?php include 'includes/navbar.php';?>
 
 <div class="container">
-        <div class="row">
+        <div class="row margin-top-20">
         <button type="button" class="btn btn-primary btn-lg btn-block loginBTN" data-toggle="modal" data-target="#tsModal" name="teamscrim">Proponer TS</button>
+        </div>
+
+        <div class="row">
+            <h3 class="h-align margin-top-20">Team Scrims disponibles</h3>
+        </div>
+
+        <div class="row">
+        <?php while($row = $listar->fetch(PDO::FETCH_ASSOC)) { ?>
+            <div class="h-align margin-top-20 ts">
+                <p>El equipo <b><?php echo $row['nombre'] ?></b> ha propuesto una team scrim en la fecha: <b><?php echo $row['fecha'] ?></b></p>
+                <button type="button" class="btn btn-success loginBTN">Aceptar TS</button>
+            </div>
+        <?php } ?>
         </div>
 </div>
 
@@ -57,12 +76,8 @@ if (isset($_POST['proponerts'])) {
       <div class="modal-body">
         <form action="teamscrim.php" method="POST">
             <div class="form-group">
-                <label for="date">Fecha</label>
-                <input type="date" class="form-control" id="date" name="date">
-            </div>
-            <div class="form-group">
-                <label for="time">Hora</label>
-                <input type="time" class="form-control" id="time" name="time">
+                <label for="date">Fecha y hora</label>
+                <input type="datetime-local" class="form-control" id="date" name="date" min="2019-05-26T00:00">
             </div>
             <button type="submit" class="btn btn-primary btn-raised" name="proponerts" value="Proponer TS">Proponer TS</button>
         </form>
