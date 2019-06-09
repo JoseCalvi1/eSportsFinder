7cb24114af2f51d5b354d70728686f82fbcf3d79
Jose Calvillo <josecalvilloolmedo@gmail.com>
2019-06-09 18:48:06 +0200

    Eliminar cuenta

--- micuenta.php
+++ micuenta.php
@@ -22,6 +22,28 @@ if (!isset($_SESSION['correo'])) {
     $sql->execute();
     $resultado = $sql->fetch(PDO::FETCH_ASSOC);
 }
+
+// Hacemos eliminar el equipo
+if (isset($_POST['eliminarcuenta'])) {
+        try {
+            $opc = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+            $dsn = "mysql:host=localhost;dbname=esports_finder";
+            $conex = new PDO($dsn, "root", "", $opc);
+            //$dsn = "mysql:host=esportsfpqjose.mysql.db;dbname=esportsfpqjose";
+            //$conex = new PDO($dsn, "esportsfpqjose", "19TEquiero2014", $opc);
+        } catch (Exception $ex) {
+          die("Error: ".$e->getMessage());
+        }
+        
+        $sql = $conex->prepare("DELETE FROM `usuarios` WHERE correo='$correo'");
+        $sql->execute();
+
+        $sql = $conex->prepare("DELETE FROM `cod` WHERE created_by='$correo'");
+        $sql->execute();
+
+        header("Location: index.php");
+    
+}
 ?>
 
 <?php include 'includes/head.php';?>
@@ -36,7 +58,11 @@ if (!isset($_SESSION['correo'])) {
     <label>Usuario: </label><p><?php echo $resultado['usuario']; ?></p>
     <label>Clave: </label><p><?php echo $resultado['clave']; ?></p>
     <label>Creado el día:</label><p><?php echo $resultado['date_entered']; ?></p>
+    <form class="formcrear margin5" action="micuenta.php" method="POST">
+        <button type="submit" class="btn btn-danger" name="eliminarcuenta">Eliminar cuenta</button>
+    </form>
 </div>
+
 
 <?php include 'includes/foot.php';?>
 
