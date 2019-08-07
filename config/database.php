3aeb2479bf145a76360d59d52ca8ae7680c02a18
aromero-vwe <aromero@vipworldevents.com>
2019-08-07 19:00:54 +0200

    Configuración de base de datos

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ return array(
     "host"      =>"localhost",
     "user"      =>"root",
     "pass"      =>"",
-    "database"  =>"esports_finder",
+    "database"  =>"esportsfinder_v2",
     "charset"   =>"utf8"
 );
 ?>
