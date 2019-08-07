fc41ba27a61101b7cab2f193aea265980a4a6227
aromero-vwe <aromero@vipworldevents.com>
2019-08-07 19:05:31 +0200

    Ficheros de configuración inicial

--- config/global.php
+++ config/global.php
@@ -1,5 +1,5 @@
 <?php
-define("CONTROLADOR_DEFECTO", "User");
+define("CONTROLADOR_DEFECTO", "Usuarios");
 define("ACCION_DEFECTO", "index");
 define("CONTROLADOR_HOME_DEFECTO", "Home");
 define("ACCION_HOME_DEFECTO", "index");
