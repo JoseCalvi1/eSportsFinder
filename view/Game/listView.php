c5ac606006cfe6e3e5cb035d1e144588e49ffd75
jcalvi <jcalvi@tourismoptimizerplatform.com>
2019-11-22 13:44:10 +0100

    Arreglos principales movil

--- view/Game/listView.php
+++ view/Game/listView.php
@@ -1,13 +1,13 @@
 <?php include_once "header.php"; ?>
 
 
-<div class="container">
+<div class="container" style="max-width: 100vw;">
     <div class="row">
 
         <?php foreach($games as $game) { ?>
             <div class="col-6 col-md-3 card-title">
                 <a href="<?php echo ($game->status)=='READY' ? $this->helper->url("Game", "home").'&id='.$game->id : '#'; ?>" class="link-title">
-                    <img class="title-img" width="180" height="250" src="assets/images/<?php echo strtolower($game->media); ?>.jpg"><br>
+                    <img class="title-img" width="150" height="230" src="assets/images/<?php echo strtolower($game->media); ?>.jpg"><br>
                     <span class="title"><?php echo $game->name; ?> / <?php echo ($game->crossplay) ? "All platforms" : $game->platform; ?></span>
                     <span class="platform"></span><br>
                     <span><?php echo $game->description; ?></span>
